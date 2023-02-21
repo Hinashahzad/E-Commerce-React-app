22cedde84834a27c3da63c856b94f7995f62a7e3
Hina Shahzad <heenashafique@gmail.com>
2023-02-21 13:55:32 +0100

    added thunk

--- src/Services/Api/ProductsApi.js
+++ src/Services/Api/ProductsApi.js
@@ -1,8 +1,7 @@
-import axios from "axios";
+import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { fetchAsyncProducts } from "../../Component/features/product/productSlice";
-
 
 const ProductsApi = () =>
 {
@@ -11,6 +10,16 @@ const ProductsApi = () =>
     {
         dispatch(fetchAsyncProducts());
     }, [dispatch] );
-    
+  
+  const isLoading = useSelector((state) => state.product.isLoading)
+  const error = useSelector((state) => state.product.error)
+
+  if (isLoading) {
+    return 'loading...'
+  }
+
+  if (error) {
+    return error
+  }
 };
 export default ProductsApi;
--- src/Services/Api/SingleProductApi.js
+++ src/Services/Api/SingleProductApi.js
@@ -1,17 +1,14 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import axios from "axios";
 import SelectProduct from "./../../Component/features/product/SelectProduct";
-import { selectProduct, removeSelectedProduct } from '../../Component/features/product/productSlice';
+import { removeSelectedProduct } from '../../Component/features/product/productSlice';
 import { useDispatch } from "react-redux";
 import { fetchAsyncSingleProduct } from '../../Component/features/product/productSlice';
-
 
 const SingleProductApi = () =>
 {
     const { productId } = useParams();
     const dispatch = useDispatch();
-    // Function to get the individual product id through axios
     
     useEffect( () =>
     {
